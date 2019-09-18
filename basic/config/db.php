978752bfdd8dc0f393c475959b1bf12057692139
Kresno <kresno.19@gmail.com>
2019-09-18 16:13:41 +0700

    edit misi error

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=es4k1p_db2017',
+    'dsn' => 'mysql:host=localhost;dbname=es4k1p_db',
     'username' => 'es4k1p_admin',
     'password' => 'pass8080word',
     'charset' => 'utf8',
